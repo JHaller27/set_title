6bbfa06e4b79e32fbe78dab07d6e9809fbc21451
james <jameshaller27@gmail.com>
2021-01-16 14:39:56 -0800

    Add --quiet

--- set_title.py
+++ set_title.py
@@ -5,12 +5,20 @@ import re
 
 
 TITLE_REGEX = re.compile(r'(?P<open_tag><[^>\w]*title[^>]*>)(?P<old_title>[^<]*)(?P<close_tag><[^>]*/[^>]*title[^>]*>)')
+QUIET = False
+
+
+def log(*args, **kwargs):
+    if not QUIET:
+        print(*args, **kwargs)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str,
                         help='Path to file to replace (glob-patterns supported)')
+    parser.add_argument('--quiet', '-q', action='store_true',
+                        help='Silence output')
     args = parser.parse_args()
 
     return args
@@ -19,9 +27,9 @@ def get_args():
 def replace_title(path_glob):
     count = 0
     for path in glob.iglob(path_glob):
-        print(f'{path} -> ', end='')
+        log(f'{path} -> ', end='')
         name = os.path.splitext(os.path.basename(path))[0]
-        print(f'"{name}"')
+        log(f'"{name}"')
 
         with open(path, 'r') as fp:
             old_text = fp.read()
@@ -31,11 +39,16 @@ def replace_title(path_glob):
         with open(path, 'w') as fp:
             fp.write(new_text)
         count += 1
-    print(f'{count} files modified')
+    log(f'{count} files modified')
 
 
 def main():
+    global QUIET
+
     args = get_args()
+
+    QUIET = args.quiet
+
     for path in glob.iglob(args.path):
         replace_title(path)
 
