3879f64b718ba208edfd4d7f37c637e63abf1142
James <jameshaller27@gmail.com>
2021-05-13 13:18:36 -0700

    Added comments and removed unnecessary loop

--- set_title.py
+++ set_title.py
@@ -9,11 +9,18 @@ QUIET = False
 
 
 def log(*args, **kwargs):
+    """
+    Simple pass-through to print(), but IFF global QUIET flag is False
+    """
+
     if not QUIET:
         print(*args, **kwargs)
 
 
 def get_args():
+    """
+    Set-up argparse and get cli arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str,
                         help='Path to file to replace (glob-patterns supported)')
@@ -24,22 +31,22 @@ def get_args():
     return args
 
 
-def replace_title(path_glob):
-    count = 0
-    for path in glob.iglob(path_glob):
-        log(f'{path} -> ', end='')
-        name = os.path.splitext(os.path.basename(path))[0]
-        log(f'"{name}"')
+def replace_title(path):
+    # Print path name, get the file name (ie the new <title> value), then print the file name
+    log(f'{path} -> ', end='')
+    name = os.path.splitext(os.path.basename(path))[0]
+    log(f'"{name}"')
 
-        with open(path, 'r') as fp:
-            old_text = fp.read()
+    # Read the file and store as text for regex replacement
+    with open(path, 'r') as fp:
+        old_text = fp.read()
 
-        new_text = TITLE_REGEX.sub(f'\\1{name}\\3', old_text)
+    # Substitute <title>filename</title> in the file's text
+    new_text = TITLE_REGEX.sub(f'\\1{name}\\3', old_text)
 
-        with open(path, 'w') as fp:
-            fp.write(new_text)
-        count += 1
-    log(f'{count} files modified')
+    # Write substituted-text back to original file
+    with open(path, 'w') as fp:
+        fp.write(new_text)
 
 
 def main():
@@ -49,6 +56,7 @@ def main():
 
     QUIET = args.quiet
 
+    # Loop over each path in the glob search pattern...
     for path in glob.iglob(args.path):
         replace_title(path)
 
